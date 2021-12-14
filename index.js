644af5cff31853c7053f7f0bcd9f4089760a3b77
jvpink200 <jvillarr20@gmail.com>
2021-12-14 15:44:10 -0800

    working shopping list with items

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const items = ["Candles", "Decorations", "Chocolate"];
-const checklist = document.getElementById("checklist");
+const checkList = document.getElementById("checklist");
+
+for(let i = 0; i < items.length; i++) {
+    checkList.innerHTML += `<div class='checklist-item'>
+                            <input type='checkbox' id='${items[i]} value='${items[i]}'>
+                            <label for='${items[i]}' class="strikethrough">${items[i]}</label>
+                            </div>`;
+                          
+}
