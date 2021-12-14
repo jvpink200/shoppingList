282eb81405fcf5ae6db7861150961334056bd9f7
jvpink200 <jvillarr20@gmail.com>
2021-12-14 15:45:49 -0800

    refactored index js

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ for(let i = 0; i < items.length; i++) {
     checkList.innerHTML += `<div class='checklist-item'>
                             <input type='checkbox' id='${items[i]} value='${items[i]}'>
                             <label for='${items[i]}' class="strikethrough">${items[i]}</label>
-                            </div>`;
-                          
+                            </div>`;                         
 }
